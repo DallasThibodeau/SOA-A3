76d02cbb9e95b83cdde3e51e93d2eca6db8bbae1
Dallas Thibodeau <fusebox45@gmail.com>
2016-10-13 19:46:07 -0400

    Added first two services. no logging/error checking.

---
+++ Loan/Loan/Loan.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Loan
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Loan" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Loan.svc or Loan.svc.cs at the Solution Explorer and start debugging.
+    public class Loan : ILoan
+    {
+        public float LoanPayment(float principleAmmount, float interestRate, int numPayments)
+        {
+            float monthlyPay = 0;
+            float denominator = (float)Math.Pow(1 + Convert.ToDouble(interestRate), Convert.ToDouble(numPayments)) - 1;
+
+            monthlyPay = interestRate + (interestRate / denominator);
+            monthlyPay *= principleAmmount;
+            return monthlyPay;
+        }
+    }
+}
