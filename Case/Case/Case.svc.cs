425d22e39a99a23bd170034b7cdb41ac24bdb58c
Dallas Thibodeau <fusebox45@gmail.com>
2016-10-13 19:34:06 -0400

    Created two services. Fun times were had. No error checking or logging integrated.

---
+++ Case/Case/Case.svc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Case
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Case" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Case.svc or Case.svc.cs at the Solution Explorer and start debugging.
+    public class Case : ICase
+    {
+        public string CaseConvert(string incomming, uint flag)
+        {
+            string toReturn = "";
+
+            if (flag == 1)
+            {
+
+            }
+            else if (flag == 2)
+            {
+                toReturn = incomming.ToUpper();
+            }
+            else
+            {
+                toReturn = incomming.ToLower();
+            }
+            return toReturn;
+        }
+    }
+}
