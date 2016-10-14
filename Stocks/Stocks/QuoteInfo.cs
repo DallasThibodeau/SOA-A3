d7464eafc07b6d17e6a7b67d16c958e0452ac3e6
Dallas Thibodeau <fusebox45@gmail.com>
2016-10-14 04:33:00 -0400

    Created last web service. It's late and it's not letting me test propperly but the most likely error is data loss casting a decimal as a double. If there are errors it'll be in Stocks.svc.cs, line 21, second argument.
    No logging or error checking.

---
+++ Stocks/Stocks/QuoteInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace Stocks
+{
+    [DataContract]
+    public class QuoteInfo
+    {
+        public string symbol;
+        public double LastPrice;
+        public string LastPriceDate;
+        public string LastPriceTime;
+
+        public QuoteInfo (string nSymbol, double nLP, string nLPD, string nLPT)
+        {
+            symbol = nSymbol;
+            LastPrice = nLP;
+            LastPriceDate = nLPD;
+            LastPriceTime = nLPT;
+        }
+    }
+
+}
---
+++ Stocks/Stocks/Stocks.svc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Stocks
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Stocks" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Stocks.svc or Stocks.svc.cs at the Solution Explorer and start debugging.
+    public class Stocks : IStocks
+    {
+        public QuoteInfo GetQuote(string tickerSymbol)
+        {
+            StockService.DelayedStockQuote serv = new StockService.DelayedStockQuote();
+
+            StockService.QuoteData response = serv.GetQuote(tickerSymbol, 0.ToString());
+
+            QuoteInfo respConverted = new QuoteInfo(response.StockSymbol, (double)response.LastTradeAmount, response.LastTradeDateTime.Date.ToString(), response.LastTradeDateTime.TimeOfDay.ToString());
+
+            return respConverted;
+        }
+    }
+}
